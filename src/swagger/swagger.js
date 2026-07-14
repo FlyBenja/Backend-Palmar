4a031f788dbe8ac6c9b181e2448ba78d69a764c2
FlyBenja <aldanabenjamin185@gmail.com>
2026-07-13 20:45:25 -0600

    Mejora para que swagger funcione en render

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,5 +1,7 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 
+const serverUrl = process.env.SERVER_URL || 'http://localhost:3000';
+
 const options = {
   definition: {
     openapi: '3.0.0',
@@ -10,8 +12,8 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
-        description: 'Servidor local'
+        url: serverUrl,
+        description: 'Servidor actual'
       }
     ],
     components: {
